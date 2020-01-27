20cecbb80c7ce84577b029f730d9ad0fded17491
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2020-01-27 17:51:02 +0100

    Added client code for JAMA API

---
+++ src/app/jama/rxjs-operators.ts
@@ -0,0 +1,10 @@
+// RxJS imports according to https://angular.io/docs/ts/latest/guide/server-communication.html#!#rxjs
+
+// See node_module/rxjs/Rxjs.js
+// Import just the rxjs statics and operators we need for THIS app.
+
+// Statics
+
+// Operators
+
+
