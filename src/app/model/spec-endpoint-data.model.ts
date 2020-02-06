56d0c95788d25380223924bf2292fa5c3438f6c3
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2020-02-06 21:24:15 +0100

    Added subentities for endpoint details

--- src/app/model/spec-endpoint-data.model.ts
+++ src/app/model/spec-endpoint-data.model.ts
@@ -1,5 +1,41 @@
-export class SpecEndpointData {
-	id?: number;
+export class SpecEndpointPathParameter {
 	name: string;
 	description: string;
+	type?: string;
+}
+
+export class SpecEndpointQueryString {
+	name: string;
+	description: string;
+	type?: string;
+	required: boolean;
+	default?: string;
+}
+
+export class SpecEndpointRequestBody {
+	description?: string;
+	mediaType: string;
+	modelId?: number;
+	modelName: string;
+	example?: string;
+}
+
+export class SpecEndpointResponse {
+	statusCode: number;
+	description: string;
+	mediaType: string;
+	modelId?: string;
+	modelName?: string;
+	example?: string;
+}
+
+export class SpecEndpointData {
+	id?: number;
+	method: string;
+	url: string;
+	summary: string;
+	pathParameters: SpecEndpointPathParameter[];
+	queryStrings: SpecEndpointQueryString[];
+	requestBody?: SpecEndpointRequestBody;
+	responses: SpecEndpointResponse[];
 }
