fac585448b6cc7a87a8122546cdf996f5780e902
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2020-02-06 21:23:58 +0100

    Implemented parsing of params and request body

--- src/app/service/endpoint.service.ts
+++ src/app/service/endpoint.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 
-import { SpecEndpointData } from '../model/spec-endpoint-data.model';
-import { OpenAPIEndpoint } from '../model/openapi.model';
+import { SpecEndpointData, SpecEndpointPathParameter, SpecEndpointQueryString, SpecEndpointRequestBody } from '../model/spec-endpoint-data.model';
+import { OpenAPIEndpoint, OpenAPIParameter, OpenAPIParameterType, OpenAPIRequestBody } from '../model/openapi.model';
 
 @Injectable({
 	providedIn: 'root'
@@ -10,18 +10,89 @@ export class EndpointService {
 
 	public parseFromOpenAPI(url: string, method: string, openAPIEndpoint: OpenAPIEndpoint): SpecEndpointData {
 
-		const name = `${method.toUpperCase()} ${url}`;
+		const endpoint: SpecEndpointData = {
+			method: method.toUpperCase(),
+			url: url,
+			summary: openAPIEndpoint.summary,
+			pathParameters: [],
+			queryStrings: [],
+			responses: []
+		};
 
-		let description = openAPIEndpoint.summary;
-		if (!!openAPIEndpoint.parameters) {
-			description += '<br><br>';
-			description += '<b>Parameters:</b><br>';
-			description += 'Pending to define parameters';
+		if (!!openAPIEndpoint.parameters)
+		{
+			this.addParametersFromOpenAPI(endpoint, openAPIEndpoint.parameters);
 		}
 
-		return {
-			name: name,
-			description: description
-		};
+		if (!!openAPIEndpoint.requestBody)
+		{
+			this.addRequestBodyFromOpenAPI(endpoint, openAPIEndpoint.requestBody);
+		}
+
+		return endpoint;
+	}
+
+	private addParametersFromOpenAPI(endpoint: SpecEndpointData, openAPIParameters: OpenAPIParameter[])
+	{
+		for (const openAPIParameter of openAPIParameters)
+		{
+			if (openAPIParameter.in === OpenAPIParameterType.Path)
+			{
+				const pathParameter: SpecEndpointPathParameter = {
+					name: openAPIParameter.name,
+					description: openAPIParameter.description
+				}
+
+				if (!!openAPIParameter.schema.type) {
+					pathParameter.type = openAPIParameter.schema.type;
+				}
+
+				endpoint.pathParameters.push(pathParameter);
+			}
+			else if (openAPIParameter.in === OpenAPIParameterType.Query)
+			{
+				const queryString: SpecEndpointQueryString = {
+					name: openAPIParameter.name,
+					description: openAPIParameter.description,
+					required: openAPIParameter.required
+				};
+
+				if (!!openAPIParameter.schema.type) {
+					queryString.type = openAPIParameter.schema.type;
+				}
+
+				if (!!openAPIParameter.schema.default) {
+					queryString.default = openAPIParameter.schema.default;
+				}
+
+				endpoint.queryStrings.push(queryString);
+			}
+		}
+	}
+
+	private addRequestBodyFromOpenAPI(endpoint: SpecEndpointData, openAPIRequestBody: OpenAPIRequestBody)
+	{
+		const mediaTypes: string[] = Object.keys(openAPIRequestBody.content);
+		if (mediaTypes.length > 0)
+		{
+			const mediaType = mediaTypes[0];
+			const schema = openAPIRequestBody.content[mediaType].schema;
+			const example = openAPIRequestBody.content[mediaType].example;
+			const schemaReference = schema["$ref"];
+			const modelName = schemaReference.startsWith("#/components/schemas/") ? schemaReference.substr(21) : schemaReference;
+
+			const requestBody: SpecEndpointRequestBody = {
+				description: openAPIRequestBody.description,
+				mediaType: mediaType,
+				modelName: modelName
+			};
+
+			if (!!example) {
+				requestBody.example = example;
+			}
+	
+			endpoint.requestBody = requestBody;
+			console.log("Endpoint Request Body", endpoint.requestBody);
+		}
 	}
 }
