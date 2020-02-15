2bdcaded31edd5fcccf9305911eefe8e12ecd753
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2020-02-15 13:48:07 +0100

    Prepared layout to display models

--- src/app/features/report/spec-model/spec-model.component.ts
+++ src/app/features/report/spec-model/spec-model.component.ts
@@ -1,14 +1,23 @@
 import { Component, Input } from '@angular/core';
 
-import { SpecModelData } from '../../../model/spec-model-data.model';
+import { SpecModelData } from '@model';
 
 
 @Component({
 	selector: 'app-spec-model',
-	templateUrl: 'spec-model.component.html'
+	templateUrl: 'spec-model.component.html',
+	styleUrls: ['spec-model.component.scss']
 })
 export class SpecModelComponent {
 
+	public bodyVisibile: boolean = false;
+
 	@Input() model: SpecModelData;
 
+	public constructor() {}
+
+	public toogleBodyVisibility()
+	{
+		this.bodyVisibile = !this.bodyVisibile;
+	}
 }
