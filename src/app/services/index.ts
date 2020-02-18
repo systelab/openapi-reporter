ed993d3dcdf7948459f5fde6726e10df0aa46c7a
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2020-02-18 09:09:32 +0100

    Registered new JAMA services into barrel

--- src/app/services/index.ts
+++ src/app/services/index.ts
@@ -1,3 +1,7 @@
-export * from "./endpoint.service";
-export * from "./model.service";
-export * from "./report.service";
+export * from './endpoint.service';
+export * from './model.service';
+export * from './report.service';
+
+export * from './jama-scanner.service';
+export * from './jama-data-type-formatter.service';
+export * from './jama-endpoint-formatter.service';
