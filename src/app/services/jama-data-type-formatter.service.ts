c851d22436a0a1f541ba0a4e9a1bfe53f6746fee
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2020-02-19 16:21:05 +0100

    Completed formatting data types

--- src/app/services/jama-data-type-formatter.service.ts
+++ src/app/services/jama-data-type-formatter.service.ts
@@ -19,6 +19,41 @@ export class JAMADataTypeFormatterService {
 			description += '<p>' + jamaDataType.data.description + '</p>';
 		}
 
+		description += '<p>It shall have the following members:</p>';
+		description += '<br>';
+		description += '<table border=\"1\" cellpadding=\"1\" cellspacing=\"1\" style=\"width:575px\">\n';
+		description += '    <tbody>\n';
+		description += '        <tr>\n';
+		description += '            <td style=\"width:120px\"><b>Name</b></td>\n';
+		description += '            <td style=\"width:60px\" style=\"text-align:center\"><b>Type</b></td>\n';
+		description += '            <td style=\"width:395px\"><b>Description</b></td>\n';
+		description += '        </tr>\n';
+
+		for (const modelProperty of jamaDataType.data.properties) {
+			description += '        <tr>\n';
+			description += '            <td style=\"width:120px\">' + modelProperty.name + '</td>\n';
+
+			description += '            <td style=\"width:60px\" style=\"text-align:center\">';
+			if (modelProperty.typeBasic || !modelProperty.typeId) {
+				description += modelProperty.typeName;
+			} else {
+				description += `<a href='perspective.req#/items/${modelProperty.typeId}'>` + modelProperty.typeName + '</a>';
+			}
+
+			if (modelProperty.typeArray) {
+				description += '[]';
+			}
+
+			description += '</td>\n';
+
+			description += '            <td style=\"width:395px\">' + modelProperty.description + '</td>\n';
+			description += '        </tr>\n';
+		}
+
+		description += '    </tbody>\n';
+		description += '</table>\n';
+		description += '<br>\n';
+
 		return description;
 	}
 }
